5fcbee1841662f8728b018e81a4c0255db510d2f
Ameer Ahmed <ameer@clara.doctor>
2021-10-22 18:45:01 -0400

    data move
    restructured source data files

--- client/src/components/avatar-card/avatar-card.tsx
+++ client/src/components/avatar-card/avatar-card.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import './avatar-card.css';
-import { ACCOUNT } from '../../data/account';
-import { PROFILE } from '../../data/profile';
+import React from "react";
+import "./avatar-card.css";
+import { ACCOUNT } from "../../data/cody/account";
+import { PROFILE } from "../../data/cody/profile";
 
 export default function AvatarCard(props: any) {
-    const { hideBio } = props;
-    const { username, accountDisplayName } = ACCOUNT.account;
-    const { avatarMediaUrl, description } = PROFILE.profile;
-    const bio = description.bio;
+  const { hideBio } = props;
+  const { username, accountDisplayName } = ACCOUNT.account;
+  const { avatarMediaUrl, description } = PROFILE.profile;
+  const bio = description.bio;
 
-
-    return (
-        <>
-            <div className="card">
-                <img alt="User Icon" className="avatar-img" src={avatarMediaUrl} />
-                <div className="account-name">{accountDisplayName}</div>
-                <div className="account-handle">@{username}</div>
-            </div>
-            { !hideBio && <p>{bio}</p> }
-        </>
-    )
+  return (
+    <>
+      <div className="card">
+        <img alt="User Icon" className="avatar-img" src={avatarMediaUrl} />
+        <div className="account-name">{accountDisplayName}</div>
+        <div className="account-handle">@{username}</div>
+      </div>
+      {!hideBio && <p>{bio}</p>}
+    </>
+  );
 }
--- client/src/components/timeline/timeline.tsx
+++ client/src/components/timeline/timeline.tsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import './timeline.css';
-import AvatarCard from '../avatar-card/avatar-card';
-import Tweet from '../tweet/tweet';
-import { TWEET_SUBSET } from '../../data/tweet-subset';
+import React from "react";
+import "./timeline.css";
+import AvatarCard from "../avatar-card/avatar-card";
+import Tweet from "../tweet/tweet";
+import { TWEET_SUBSET } from "../../data/cody/tweet";
 
 export default function Timeline() {
-
-
-    return (
-        <div className="container">
-            <div className="left-col">
-                <AvatarCard/>
-                Date generated·March 4, 2021 at 4:03:52 PM GMT-8·Estimated size·62 MB
-                <div className="btn-stack">
-                    <a className="btn rounded-btn">Home</a><br/>
-                    <a className="btn rounded-btn">Account</a><br/>
-                    <a className="btn rounded-btn">Tweets</a><br/>
-                </div>
-            </div>
-            <div className="feed-col">
-                { TWEET_SUBSET.map(tweet => {
-                    return (<Tweet tweet={tweet.tweet}/>)})
-                }
-            </div>
-            <div className="metadata-col">
-                {/* Metadata */}
-            </div>
+  return (
+    <div className="container">
+      <div className="left-col">
+        <AvatarCard />
+        Date generated·March 4, 2021 at 4:03:52 PM GMT-8·Estimated size·62 MB
+        <div className="btn-stack">
+          <a className="btn rounded-btn">Home</a>
+          <br />
+          <a className="btn rounded-btn">Account</a>
+          <br />
+          <a className="btn rounded-btn">Tweets</a>
+          <br />
         </div>
-    );
+      </div>
+      <div className="feed-col">
+        {TWEET_SUBSET.map((tweet) => {
+          return <Tweet tweet={tweet.tweet} />;
+        })}
+      </div>
+      <div className="metadata-col">{/* Metadata */}</div>
+    </div>
+  );
 }
