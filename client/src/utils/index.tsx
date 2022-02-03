740eb2c4c8d77f11e8a1ef7d112d2c80d780be40
Ameer Ahmed <ameer@clara.doctor>
2022-02-03 02:36:48 -0500

    Working implementation of writing and reading references for uploaded archives
    - updated Beejs upload method to save profile object in Swarm, utilizing feeds and single owner chunks (soc)
    - compress and decompress message using lz-string
    - convenient methods to write and read and feeds and socs
    - update gitignore for .env

--- client/src/utils/index.tsx
+++ client/src/utils/index.tsx
@@ -1,3 +1,5 @@
+import { Utils } from "@ethersphere/bee-js";
+
 /**
  * Format bytes as human-readable text.
  *
@@ -6,12 +8,11 @@
  *
  * @return Formatted string.
  */
-// @ts-ignore
-export const convertBytesToString = (bytes, dp = 1) => {
+export const convertBytesToString = (bytes: number, dp: number = 1) => {
   const thresh = 1000;
 
   if (Math.abs(bytes) < thresh) {
-    return bytes + " B";
+    return bytes + " Bytes";
   }
 
   const units = ["KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
@@ -24,4 +25,36 @@ export const convertBytesToString = (bytes, dp = 1) => {
   } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
 
   return bytes.toFixed(dp) + " " + units[u];
+};
+
+/**
+ * Calculates size of a JSON object.
+ *
+ * @param json JSON object.
+ *
+ * @return Size of JSON object formatted as a string.
+ *
+ */
+export const getJsonSize = (json: any) => {
+  const size = new TextEncoder().encode(JSON.stringify(json)).length;
+  return convertBytesToString(size);
+};
+
+/**
+ * Converts Swarm's feedIndex to integer.
+ *
+ * @param feedIndex feedIndex string value e.g. 0000000000000001
+ *
+ * @return Converted integer value
+ *
+ */
+export const convertFeedIndexToInt = (feedIndex: string): number => {
+  const indexBytes = Utils.hexToBytes(feedIndex);
+  let index = 0;
+  for (let i = indexBytes.length - 1; i >= 0; i--) {
+    const byte = indexBytes[i];
+    if (byte === 0) break;
+    index += byte;
+  }
+  return index;
 };
