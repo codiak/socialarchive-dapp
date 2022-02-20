39d8ef442f78fa4538c43d86e799357f3299641f
Jonathan Phillips <jon_m_phillips@yahoo.com>
2022-02-20 15:15:46 -0500

    Issue #53 Issue #54 - basic page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,58 +2,90 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UploadPage from "./components/upload-page/upload-page";
 import BrowsePage from "./components/browse-page/browse-page";
 import ArchivePage from "./components/archive-page/archive-page";
+import CookiesPage from "./components/cookies-page/cookies-page";
 import ArchiveDownload from "./components/archive-download/archive-download";
 import "./App.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import { StoreProvider } from "./utils/store";
-
+import { useState } from "react";
+import { CookieConsentProvider } from "./components/cookie-consent/cookie-consent";
+// import { Modal } from "./components/modal/modal";
 const APP_TITLE = "Social Archive";
 
 function App() {
-  const activeLink = (pathname: string, startsWith?: boolean) => {
-    let isActive = (startsWith && window.location.pathname.startsWith(pathname)) || window.location.pathname === pathname;
-    return "menu-link " + (isActive ? "active" : "");
-  };
-
-  return (
-    <div className="wrapper">
-      <HelmetProvider>
-        <Helmet>
-          <title>Social Archive</title>
-          <meta name="description" content="Twitter back-up on the blockchain" />
-        </Helmet>
-        <header className="App-header">
-          <a className="header-logo-link" href="/">
-            <img className="header-logo" src={process.env.PUBLIC_URL + "/logo.png"} alt={APP_TITLE} />
-          </a>
-          <div className="menu">
-            <a className={activeLink("/")} href="/">
-              Upload
-            </a>
-            <a className={activeLink("/browse", true)} href="/browse">
-              Browse
-            </a>
-          </div>
-          <title>{APP_TITLE}</title>
-        </header>
-        <StoreProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/browse" element={<BrowsePage />}>
-                <Route path=":section" element={<BrowsePage />} />
-              </Route>
-              <Route path="/archive" element={<ArchivePage />}>
-                <Route path=":user/:section" element={<ArchivePage />} />
-              </Route>
-              <Route path="/archive/:id" element={<ArchiveDownload />} />
-              <Route path="/" element={<UploadPage />} />
-            </Routes>
-          </BrowserRouter>
-        </StoreProvider>
-      </HelmetProvider>
-    </div>
-  );
+    const activeLink = (pathname: string, startsWith?: boolean) => {
+        let isActive =
+            (startsWith && window.location.pathname.startsWith(pathname)) ||
+            window.location.pathname === pathname;
+        return "menu-link " + (isActive ? "active" : "");
+    };
+    return (
+        <div className="wrapper">
+            <CookieConsentProvider>
+                <HelmetProvider>
+                    <Helmet>
+                        <title>Social Archive</title>
+                        <meta
+                            name="description"
+                            content="Twitter back-up on the blockchain"
+                        />
+                    </Helmet>
+                    <header className="App-header">
+                        <a className="header-logo-link" href="/">
+                            <img
+                                className="header-logo"
+                                src={process.env.PUBLIC_URL + "/logo.png"}
+                                alt={APP_TITLE}
+                            />
+                        </a>
+                        <div className="menu">
+                            <a className={activeLink("/")} href="/">
+                                Upload
+                            </a>
+                            <a
+                                className={activeLink("/browse", true)}
+                                href="/browse"
+                            >
+                                Browse
+                            </a>
+                        </div>
+                        <title>{APP_TITLE}</title>
+                    </header>
+                    <StoreProvider>
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="/browse" element={<BrowsePage />}>
+                                    <Route
+                                        path=":section"
+                                        element={<BrowsePage />}
+                                    />
+                                </Route>
+                                <Route
+                                    path="/archive"
+                                    element={<ArchivePage />}
+                                >
+                                    <Route
+                                        path=":user/:section"
+                                        element={<ArchivePage />}
+                                    />
+                                </Route>
+                                <Route
+                                    path="/archive/:id"
+                                    element={<ArchiveDownload />}
+                                />
+                                <Route path="/" element={<UploadPage />} />
+                                <Route
+                                    path="/cookies"
+                                    element={<CookiesPage />}
+                                />
+                            </Routes>
+                        </BrowserRouter>
+                    </StoreProvider>
+                </HelmetProvider>
+            </CookieConsentProvider>
+        </div>
+    );
 }
 
 export default App;
