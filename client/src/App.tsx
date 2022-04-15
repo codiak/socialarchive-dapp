b71db990a496aa9751a57c8ee97f6fb13bbf2b00
Jonathan Phillips <jon_m_phillips@yahoo.com>
2022-04-15 17:45:41 -0400

    Added GDPR modal Added Cookies Policy Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import HomePage from "./components/home-page/home-page";
 import UploadPage from "./components/upload-page/upload-page";
 import BrowsePage from "./components/browse-page/browse-page";
 import ArchivePage from "./components/archive-page/archive-page";
+import CookiesPage from "./components/cookies-page/cookies-page";
 import ArchiveDownload from "./components/archive-download/archive-download";
 import Footer from "./components/footer/footer";
 import "./App.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import { StoreProvider } from "./utils/store";
+import { CookieConsentProvider } from "./components/cookie-consent/cookie-consent";
 
 const APP_TITLE = "Social Archive";
 
@@ -22,54 +24,57 @@ function App() {
 
   return (
     <div className="wrapper">
-      <HelmetProvider>
-        <Helmet>
-          <title>Social Archive</title>
-          <meta name="description" content="Twitter back-up on the blockchain" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
-        </Helmet>
-        <header className="App-header">
-          <a className="header-logo-link" href="/">
-            <img
-              className="header-logo"
-              src={process.env.PUBLIC_URL + "/logo.png"}
-              alt={APP_TITLE}
-            />
-          </a>
-          <div className="menu">
-            <a className={activeLink("/upload")} href="/upload">
-              Upload
+      <CookieConsentProvider>
+        <HelmetProvider>
+          <Helmet>
+            <title>Social Archive</title>
+            <meta name="description" content="Twitter back-up on the blockchain" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
+          </Helmet>
+          <header className="App-header">
+            <a className="header-logo-link" href="/">
+              <img
+                className="header-logo"
+                src={process.env.PUBLIC_URL + "/logo.png"}
+                alt={APP_TITLE}
+              />
             </a>
-            <a className={activeLink("/browse", true)} href="/browse">
-              Browse
-            </a>
-          </div>
-          <title>{APP_TITLE}</title>
-        </header>
-        <StoreProvider>
-          <BrowserRouter>
-            <div className="main-container">
-              <Routes>
-                <Route path="/browse" element={<BrowsePage />}>
-                  <Route path=":section" element={<BrowsePage />} />
-                </Route>
-                <Route path="/archive" element={<ArchivePage />}>
-                  <Route path=":user/:section" element={<ArchivePage />} />
-                </Route>
-                <Route path="/archive/:id" element={<ArchiveDownload />} />
-                <Route path="/upload" element={<UploadPage />} />
-                <Route path="/" element={<HomePage />} />
-              </Routes>
+            <div className="menu">
+              <a className={activeLink("/upload")} href="/upload">
+                Upload
+              </a>
+              <a className={activeLink("/browse", true)} href="/browse">
+                Browse
+              </a>
             </div>
-            <Footer />
-          </BrowserRouter>
-        </StoreProvider>
-      </HelmetProvider>
+            <title>{APP_TITLE}</title>
+          </header>
+          <StoreProvider>
+            <BrowserRouter>
+              <div className="main-container">
+                <Routes>
+                  <Route path="/browse" element={<BrowsePage />}>
+                    <Route path=":section" element={<BrowsePage />} />
+                  </Route>
+                  <Route path="/archive" element={<ArchivePage />}>
+                    <Route path=":user/:section" element={<ArchivePage />} />
+                  </Route>
+                  <Route path="/archive/:id" element={<ArchiveDownload />} />
+                  <Route path="/upload" element={<UploadPage />} />
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/cookies" element={<CookiesPage />} />
+                </Routes>
+              </div>
+              <Footer />
+            </BrowserRouter>
+          </StoreProvider>
+        </HelmetProvider>
+      </CookieConsentProvider>
     </div>
   );
 }
---
+++ client/src/components/cookies-page/cookies-page.tsx
@@ -0,0 +1,94 @@
+import "./cookies-page.css";
+import { Helmet } from "react-helmet-async";
+import { useEffect } from "react";
+
+export const CookiesPage = () => {
+  useEffect(() => {}, []);
+  return (
+    <div className="policy-container">
+      <Helmet>
+        <title>Privacy - Social Archive</title>
+      </Helmet>
+      <h3>Privacy & Cookies Policy</h3>
+      <h4>Information We Collect</h4>
+      <p>
+        Aside from the material uploaded to Swarm by the user, we collect website analytics for the
+        purpose of improving performance of our website.
+      </p>
+      <h4>Lawful Usage</h4>
+      <p>We only process information where we have a lawful basis for doing so which includes:</p>
+      <ul>
+        <li>
+          User consent - This is where you (the "user") has given us permission to process
+          information for a given purpose such as uploading social media to Swarm.
+        </li>
+        <li>
+          Business purposes - This is where we have a legitimate reason to collect informaton about
+          who is visiting our website and how they are using so that we can make improvements and
+          continue to provide our services.
+        </li>
+      </ul>
+      <h4>Retention</h4>
+      <p>
+        Aside from cookies stored on your machine and information transmitted to Swarm, we retain no
+        information beyond site analytics.
+      </p>
+      <h4>Questions</h4>
+      <p>
+        If you have any questions pertaining to privacy on this site, you can send an e-mail to{" "}
+        <a href="mailto:privacy@socialarchive.info">privacy@socialarchive.info</a>
+      </p>
+      <h4>Policy Changes</h4>
+      <p>Any changes to this policy will be posted on this page.</p>
+      <h4>Cookies</h4>
+      <p>
+        Social Archive uses cookies and related technologies to collect information about you. This
+        information is collected for the purposes of analytics and personalization. For information
+        about how cookies are managed by various browsers see{" "}
+        <div className="emphasis">Manage Cookies</div> below.
+      </p>
+      <p>
+        Cookies are small blocks of information stored on your device that are required by websites
+        to operate as designed. "First-party" cookies are cookies that are set by the operator of
+        this website. "Third-party" cookies are cookies that are set by this site's partners.
+      </p>
+      <h4>Types of Cookies Used</h4>
+      <ul>
+        <li>
+          Analytics - At present the only type of cookie that we currently use are cookies for the
+          purpose of traffic analysis.{" "}
+        </li>
+      </ul>
+      <p></p>
+      <h4>Manage Cookies</h4>
+      <p>
+        You can control which cookies are allowed on your device, but be aware that if you do
+        disable cookies you may impact the functioning of the site as intended. Some cookies are
+        essential for basic operations.
+      </p>
+      <p>
+        All modern web browsers allow changes to your cookie settings. These settings can be
+        typically found by clicking 'Settings' or 'Help'. The links below may also be helpful in
+        explaining how to control cookies in your browser.
+      </p>
+      <ul>
+        <li>
+          <a href="https://support.microsoft.com/en-us/microsoft-edge/view-cookies-in-microsoft-edge-a7d95376-f2cd-8e4a-25dc-1de753474879">
+            Microsoft Edge
+          </a>
+        </li>
+        <li>
+          <a href="http://support.mozilla.com/en-US/kb/Cookies">Firefox</a>
+        </li>
+        <li>
+          <a href="https://support.google.com/chrome/answer/95647?hl=en">Chrome</a>
+        </li>
+        <li>
+          <a href="https://support.apple.com/guide/safari/manage-cookies-sfri11471/mac">Safari</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default CookiesPage;
